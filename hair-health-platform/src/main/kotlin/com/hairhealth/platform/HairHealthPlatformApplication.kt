f6bbf1c7115dd9d2226d0ef88e3a40e10d297345
Sriram Durbha <sriram.durbha@gmail.com>
2025-05-29 18:17:01 -0700

    Initial

---
+++ hair-health-platform/src/main/kotlin/com/hairhealth/platform/HairHealthPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.hairhealth.platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HairHealthPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<HairHealthPlatformApplication>(*args)
+}
---
+++ hair-health-platform/src/test/kotlin/com/hairhealth/platform/HairHealthPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hairhealth.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HairHealthPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
